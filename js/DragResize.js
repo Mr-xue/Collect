ce3d79d61ccf47479c2934672cd59e006a98761e
薛崇伟 <360240422@qq.com>
2018-09-17 14:28:26 +0800

    test

--- js/DragResize.js
+++ js/DragResize.js
@@ -14,6 +14,8 @@ class DragResize {
 
 		//鼠标是否按下
 		this.mousedown = false;
+		// 是否是调节大小
+		this.resize = false;
 
 		// 存储元素坐标
 		this.x = 0;
@@ -29,12 +31,11 @@ class DragResize {
 	// 初始化
 	init (){
 		this.appendPoint();
-		this.confirm();
 		this.eleDrag();
 		this.eleResize();
 	}
 
-	// 追加调节用于调节大小的8个点
+	// 追加拖动调节大小的8个point
 	appendPoint (){
 		Array.prototype.forEach.call(this.dom, (item,index)=>{
 			// 追加缩放的点
@@ -50,49 +51,89 @@ class DragResize {
 		})
 	}
 
-	// 确认是拖动还是调节大小
-	confirm (){
-
-	}
-
 	// 元素拖动
 	eleDrag (){
-		let _self = this, $el;
-		
+		let _self = this,$el;
 		// 元素内部偏移量
 		let posX, posY;
 
 		// 鼠标点击
-		$(this.selector)[0].addEventListener('mousedown',function(e){
+		this.$el[0].addEventListener('mousedown',function(e){
 			posX = e.pageX - $(this).offset().left;
 			posY = e.pageY - $(this).offset().top;
 			$el = $(this);
-			if(!$el.hasClass('drag-point')){
-				_self.mousedown = true;
-			}
-		})
+			console.log(666);
+			_self.mousedown = true;
+
+			// 鼠标移动
+			_self.mouseMove(posX,posY,$el);
+		},false)
 
 		// 鼠标松开
 		$(document).on('mouseup',function(e){
 			_self.mousedown = false;
 		})
+	}
 
+	// 拖拽调节大小
+	eleResize (){
+		let _self = this,$el;
+		let posX, posY;
+		this.point.forEach(function(item,index){
+			$('.'+item)[0].addEventListener('mousedown',function(e){
+				posX = e.pageX - $(this).offset().left;
+				posY = e.pageY - $(this).offset().top;
+				$el = $(this);	
+				console.log(777);
+				e.stopPropagation();
+				_self.resize = true;
+				_self.mouseMove(posX,posY,$el)
+			})
+		},false)
+	}
+
+	// 鼠标移动
+	mouseMove(posX,posY,$el){
+		let _self = this;
 		// 鼠标移动
 		$(document).on('mousemove',function(e){
 			if(_self.mousedown){
 				this.x = e.pageX - posX;
 				this.y = e.pageY - posY;
 				$el.css({left:this.x+'px', top:this.y+'px'})
+			}else if(_self.resize){
+				dragResize(posX,posY,$el,selector,event)
 			}
 		})
 	}
 
-	// 拖拽调节大小
-	eleResize (){
-		$(".drag-point").each(function(index,item){
-			$(this)[0].addEventListener('mousedown',function(e){
-				e.stopPropagation();
-			})
-		})
+	// 8向拖动调节
+	dragResize (posX,posY,$el,selector,event){
+		switch (selector){
+			// 左上
+			case 'top-left':;
+				break;
+			// 上中
+			case 'top-center':;
+				break;
+			// 上右
+			case 'top-right':;
+				break;
+			// 左中
+			case 'left-center':;
+				break;
+			// 右中
+			case 'right-center':;
+				break;
+			// 下左
+			case 'bottom-left':;
+				break;
+			// 下中
+			case 'bottom-center':;
+				break;
+			// 下右
+			case 'bottom-right':;
+				break;
+		}
 	}
 }
