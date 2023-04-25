81347ea73825ec49e85e4f7366ac4a2dbfb79682
菠菜 <360240422@qq.com>
2023-04-25 15:11:03 +0800

    视差展示首页

--- 展示首页/js/index.js
+++ 展示首页/js/index.js
@@ -3,59 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
-            pageWidth: 0
+            pageWidth: 0, //页面宽度
+            listType:'web', 
         };
     },
     mounted() {
-        this.getSize();
-        const myAtropos = Atropos({
-            el: '.my-atropos'
-            // rest of parameters
-        });
-        // this.addMouseEvent();
+        // 获取页面宽度
+        this.pageWidth = document.body.offsetWidth;
+        // 实例化视差插件
+        Atropos({ el: '.my-atropos' });
     },
     methods: {
-        getSize() {
-            this.pageWidth = document.body.offsetWidth;
-        },
-        addMouseEvent() {
-            let wrap = document.querySelector('.lang'),
-                wrapInfo = wrap.getBoundingClientRect(),
-                left = wrapInfo.left,
-                top = wrapInfo.top,
-                width = wrap.offsetWidth,
-                height = wrap.offsetHeight;
-
-            wrap.addEventListener('mousemove', function (e) {
-                const { clientX, clientY } = e;
-                // 最大旋转角度
-                const maxRotate = 20;
-
-                // 原点位置
-                const centerX = width / 2;
-                const centerY = height / 2;
-
-                // 鼠标位置相对卡片左上角的坐标
-                const offsetX = clientX - left;
-                const offsetY = clientY - top;
-
-                // 将和原点的距离线性折算成旋转角度
-                // 📢 X 方向移动旋转 Y 轴，Y 方向移动旋转 X 轴
-                let rotateX =
-                    ((maxRotate * (offsetY - centerY)) / centerY) * -1;
-                let rotateY = (maxRotate * (offsetX - centerX)) / centerX;
-                rotateX = Math.min(maxRotate, rotateX);
-                rotateY = Math.min(maxRotate, rotateY);
-                // 设置 CSS Variables
-                wrap.style.setProperty('--rotateX', rotateX);
-                wrap.style.setProperty('--rotateY', rotateY);
-            });
-
-            wrap.addEventListener('mouseleave', function (e) {
-                wrap.style.setProperty('--rotateX', 0);
-                wrap.style.setProperty('--rotateY', 0);
-            });
+        showList(type) {
+            this.listType = type;
         }
     }
 }).mount('#app');
